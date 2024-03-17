3aef6cd1e5dab4483b40cd066d20ab876e560557
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2024-03-17 12:39:42 +0530

    create bootcamp routes and controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true });
+};
+
+// @desc      Get single bootcamp
+// @route     GET /api/v1/bootcamps/:id
+// @access    Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true });
+};
+
+// @desc      Create new bootcamp
+// @route     POST /api/v1/bootcamps
+// @access    Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true });
+};
+
+// @desc      Update bootcamp
+// @route     PUT /api/v1/bootcamps/:id
+// @access    Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true });
+};
+
+// @desc      Delete bootcamp
+// @route     DELETE /api/v1/bootcamps/:id
+// @access    Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true });
+};
--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const dotenv = require("dotenv");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 3000;
 
