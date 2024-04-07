5a2ede3b813ceace216a83515d4384db692cce52
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2024-04-08 01:37:16 +0530

    fix delete

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,6 @@ const {
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse).put(updateCourse).delet(deleteCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
