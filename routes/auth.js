26d5eb1c735af501503432f07272fe9ef1482e18
Uthpala Samarakoon <uthpalakasun7@gmail.com>
2024-04-13 21:06:36 +0530

    updatedetails & updatepassword

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
-  // updateDetails,
-  // updatePassword
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -18,8 +18,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/me", protect, getMe);
-// router.put("/updatedetails", protect, updateDetails);
-// router.put("/updatepassword", protect, updatePassword);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
 
